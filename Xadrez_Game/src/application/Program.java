7916ea4c5d288b37bf2fbc89296fc69d6a69c84f
Garchy <byel20102010@gmail.com>
2020-12-31 17:06:15 -0300

    avancos e melhorias

--- Xadrez_Game/src/application/Program.java
+++ Xadrez_Game/src/application/Program.java
@@ -1,14 +1,45 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
 		
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
-
+		
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.print("\nSource: ");
+				ChessPosition source = UI.readChessPosition(ler);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+				
+				System.out.print("\nTarget: ");
+				ChessPosition target = UI.readChessPosition(ler);
+				
+				ChessPiece capturedChessPiece = chessMatch.perfomChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				ler.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				ler.nextLine();
+			}
+		}
 	}
 
 }
--- Xadrez_Game/src/board/Position.java
+++ Xadrez_Game/src/board/Position.java
@@ -33,5 +33,10 @@ public class Position {
 	public String toString() {
 		return row + ", " + column;
 	}
+	
+	public void setValue(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 			
 }
